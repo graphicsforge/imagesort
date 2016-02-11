9c6fc7b668ee06ea3ef0d2080118c88e71f7a023
default <graphicsforge@gmail.com>
2016-02-11 15:08:52 -0500

    add helper scripts

---
+++ run.sh
@@ -0,0 +1,2 @@
+./caffe/build/examples/cpp_classification/classification deploy.prototxt snapshot_iter_561.caffemodel labeled_images/train_mean.binaryproto labels.txt ./labeled_images/val/cat0.jpeg 
+
